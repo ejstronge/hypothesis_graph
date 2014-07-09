7a5faaff9b37ad762e211c9ad6ae9ab70e1e422f
Edward J. Stronge <ejstronge@gmail.com>
2014-07-09 09:38:47 -0400

    psycopg2 installs successfully on dev VM

--- manifests/default.pp
+++ manifests/default.pp
@@ -13,39 +13,56 @@ class { 'python':
     gunicorn => false,
 }
 
-python::virtualenv { '/vagrant/hypothesis_graph':
+python::virtualenv { '/vagrant':
     ensure => present,
     version => 'system',
-    requirements => '/var/www/hypothesis_graph_www/requirements.txt',
+    requirements => '/vagrant/requirements.txt',
     systempkgs => true,
     distribute => false,
     owner => 'root',
     group => 'admin',
-    cwd => '/var/www/hypothesis_graph_www',
+    cwd => '/vagrant',
     timeout => 1800,
 }
 
 # Postgresql
 
+package { ['postgresql-server-dev-9.3']:
+  ensure  => 'installed',
+  before  => Class['postgresql::server']
+}
+
 class { 'postgresql::globals':
     version => '9.3',
-    manage_package_repo => true,
     encoding => 'UTF8',
     locale => 'en_US.utf8',
+    server_package_name => 'postgresql-9.3',
+    contrib_package_name => 'postgresql-contrib-9.3',
 }->
 class { 'postgresql::server':
-    ensure => 'present',
+    package_ensure => 'installed',
     listen_addresses => '*',
     manage_firewall => true,
 }
 
-class { 'postgresql::server::contrib':
-    package_ensure => 'present',
+# XXX SET NEW VALUE WHEN THIS GOES ONLINE
+postgresql::server::role { 'hg':
+  password_hash => postgresql_password('hg', 'hgtest'),
+  superuser => true,
 }
+
+# System packages
 
 exec { 'apt-get update':
     command => '/usr/bin/apt-get update',
     path => 'bin',
+}
+
+$lxml_reqs = ['libxml2-dev', 'libxslt1-dev']
+package { $lxml_reqs: ensure => 'latest' }
+
+package { 'iptables-persistent': 
+    ensure => 'latest',
 }
 
 file { '/var/www/hypothesis_graph_www':
